3e1b8954ec6357c8a91ac2225be5e8787325ea74
Benito <bbxblack@gmail.com>
2021-05-17 23:01:01 +0200

    v0.2

--- MyLaravel/resources/views/pages/home.blade.php
+++ MyLaravel/resources/views/pages/home.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
 
+    <div class="container">
 
+        <img src="{{ asset ('/storage/assets/jumbotron.jpg/') }}">
+
+     <div class="row current">
+
+        <div class="lt-title">
+
+            
+
+        </div>
+
+     </div>
+    </div>
 
 @endsection
 
