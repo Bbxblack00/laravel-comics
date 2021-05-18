b26d5c37a0fd443b976a2444b9ac66c00add2041
Benito <bbxblack@gmail.com>
2021-05-18 18:21:38 +0200

    v0.5

--- MyLaravel/resources/views/pages/home.blade.php
+++ MyLaravel/resources/views/pages/home.blade.php
@@ -2,7 +2,25 @@
 
 @section('content')
 
-    
+    <main>
+
+        <section class="container main">
+            <div class="tit-home">
+                <h1>
+                    CURRENT SERIES
+                </h1>
+            </div>
+
+            @foreach ($comics as $comic)
+
+            <div class="card" style="background-image: url({{ $comic['thumb']}})">
+                <h3>{{ $comic['title'] }}</h3>
+            </div>
+                
+            @endforeach
+        </section>
+
+    </main>
 
 @endsection
 
