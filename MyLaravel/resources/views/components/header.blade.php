b1c8dd49d0bc25652e3fedccffbf1945118b0818
Benito <bbxblack@gmail.com>
2021-05-18 16:33:22 +0200

    v0.4

---
+++ MyLaravel/resources/views/components/header.blade.php
@@ -0,0 +1,93 @@
+<header>
+    <div class="container hed">
+
+
+
+        <div class="row">
+
+            <div class="logo">
+                <img src="{{ asset ('/storage/asssets/dc-logo.png ') }}" alt="">
+            </div>
+
+            <div class="menu">
+
+                <ul class="lst-menu">
+                    <li>
+                        <a href="#">
+                            CHARACTERS
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            COMICS
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            MOVIES
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            TV
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            GAMES
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            COLLECTIBLES
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            VIDEOS
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            FANS
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            NEWS
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            SHOP
+                        </a>
+                    </li>
+    
+                </ul>
+
+            </div>
+
+            <div class="search">
+                <input type="text" placeholder="cerca">
+            </div>
+
+            
+        </div>
+
+        <div class="row">
+
+            <div class="jt"></div>
+
+        </div>
+
+    </div>
+</header>
--- MyLaravel/resources/views/pages/home.blade.php
+++ MyLaravel/resources/views/pages/home.blade.php
@@ -2,22 +2,7 @@
 
 @section('content')
 
-    <div class="container">
-
-        <img src="{{ asset ('/storage/assets/jumbotron.jpg/') }}">
-
-     <div class="row current">
-
-        <div class="lt-title">
-
-            @foreach ($collection as $item)
-                
-            @endforeach
-
-        </div>
-
-     </div>
-    </div>
+    
 
 @endsection
 
