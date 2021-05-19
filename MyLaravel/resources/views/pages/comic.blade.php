d442f3bf091b09071cc876fd5863378c3b7a1bac
Benito <bbxblack@gmail.com>
2021-05-19 15:59:44 +0200

    v0.5

---
+++ MyLaravel/resources/views/pages/comic.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+    <main>
+
+        <section class="container main">
+            <div class="tit-home">
+                <h1>
+                    CURRENT SERIES
+                </h1>
+            </div>
+
+    </main>
+
+@endsection
--- MyLaravel/routes/web.php
+++ MyLaravel/routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'ControllerHome@home')
     -> name('home');
 
-Route::get('/elem/{id}', 'ControllerHome@home')
+Route::get('/elem/{id}', 'ControllerHome@elem')
     -> name('elem');
