382a36a48e2496e758d79873ef0419ca65a1732f
Benito <bbxblack@gmail.com>
2021-05-19 17:44:32 +0200

    v0.7

--- MyLaravel/resources/views/pages/comic.blade.php
+++ MyLaravel/resources/views/pages/comic.blade.php
@@ -4,9 +4,21 @@
 
     <main>
 
-        <section class="container main">
+        <section class="container main elem">
             <div class="tit-home">
                 <div class="card" style="background-image: url({{ $elem['thumb']}})">
+                </div>
+            </div>
+
+            <div class="row description">
+                <div class="row">
+                    <h1>{{ $elem['title'] }}</h1>
+
+                    <div>
+                        {{-- da sistemare con il prezzo e la formattazione --}}
+                    </div>
+
+                    <p>{{ $elem['description'] }}</p>
                 </div>
             </div>
 
